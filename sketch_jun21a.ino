5c1a31cff5f0f87a3c1a8061f515d58b324c993f
tnt2017 <tnt2011@bk.ru>
2018-06-21 16:11:03 +0700

    Add files via upload

---
+++ sketch_jun21a.ino
@@ -0,0 +1,40 @@
+#include <LiquidCrystalRus.h>
+//#include <LiquidCrystal.h>
+
+// инициализируем объект-экран, передаём использованные 
+// для подключения контакты на Arduino в порядке:
+// RS, E, DB4, DB5, DB6, DB7
+LiquidCrystalRus lcd(12, 11, 5, 4, 3, 2);
+
+ int val = 0;
+
+void setup()
+{
+    Serial.begin(9600); 
+    
+    // устанавливаем размер (количество столбцов и строк) экрана
+    lcd.begin(16, 2);
+    // печатаем первую строку
+    lcd.print("АБВГДЕЁЖЗИЙКЛМНО");
+
+    // устанавливаем курсор в колонку 0, строку 1
+    // на самом деле это вторая строка, т.к. нумерация начинается с нуля
+    lcd.setCursor(0, 1);
+    // печатаем вторую строку
+    lcd.print("Do It Yourself");
+}
+ 
+void loop()
+{
+  String inString;
+
+  while(Serial.available()) {
+    String ttydata = Serial.readString(); // ttyData - информация с серийного порта
+    Serial.print("String: ");
+    Serial.println(ttydata);
+    lcd.begin(16, 2);
+    lcd.print(ttydata);
+}
+    
+
+}
